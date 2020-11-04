9c35d7e70553056ab71909d2173fe2ac2a928b57
Sergey <prosviryakov.sergey@gmail.com>
2020-11-04 22:03:47 +0600

    int to words done

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -14,7 +14,6 @@
             maxValue = 999 :
             maxValue;
 
-        
    
     
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
@@ -27,7 +26,12 @@ const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
 
-answerField.innerText = `Вы загадали число ${answerNumber }  ?`;
+// answerField.innerText = `Вы загадали число ${replace} ?`;
+(answerNumber>=0) ?
+answerField.innerText = 'Вы загадали число ' + numToWords(answerNumber) + ' ?'
+:
+    answerField.innerText = 'Вы загадали число ' + minNumToWords(answerNumber) + ' ?';
+
 
 
 document.getElementById('btnRetry').addEventListener('click', function () {
@@ -53,7 +57,7 @@ document.getElementById('btnRetry').addEventListener('click', function () {
             gameRun = true;
     
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            answerField.innerText = 'Вы загадали число ' + numToWords(answerNumber) + ' ?';
         }
     
     })
@@ -76,14 +80,27 @@ document.getElementById('btnOver').addEventListener('click', function () {
             orderNumberField.innerText = orderNumber;
             const overPhraseRandom = Math.round(Math.random() * 4);
             const overAnswerPhrase = (overPhraseRandom === 1) ?
-                `Вы загадали число ${answerNumber }?` : (overPhraseRandom === 2) ?
-                `Я почти у верен, вы загадали число ${answerNumber }?` : (overPhraseRandom === 3) ?
-                `Ну и число вы загадали, это ${answerNumber }?` :
-                `Так я могу и проиграть, это число ${answerNumber }?`;
+            (answerNumber>=0) ?
+            answerField.innerText = 'Вы загадали число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Вы загадали число ' + minNumToWords(answerNumber) + ' ?' : (overPhraseRandom === 2) ?
+            (answerNumber>=0) ?
+            answerField.innerText = 'Я почти у верен, вы загадали число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Я почти у верен, вы загадали число ' + minNumToWords(answerNumber) + ' ?' : (overPhraseRandom === 3) ?
+            (answerNumber>=0) ?
+            answerField.innerText = 'Ну и число вы загадали, это ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Ну и число вы загадали, это ' + minNumToWords(answerNumber) + ' ?' :
+            (answerNumber>=0) ?
+            answerField.innerText = 'Так я могу и проиграть, это число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Так я могу и проиграть, это число ' + minNumToWords(answerNumber) + ' ?';
             answerField.innerText = overAnswerPhrase;
         }
     }
 })
+
 
 document.getElementById('btnLess').addEventListener('click', function () {
     if (gameRun) {
@@ -102,14 +119,27 @@ document.getElementById('btnLess').addEventListener('click', function () {
             orderNumberField.innerText = orderNumber;
             const lessPhraseRandom = Math.round(Math.random() * 4);
             const lessAnswerPhrase = (lessPhraseRandom === 1) ?
-                `Вы загадали число ${answerNumber }?` : (lessPhraseRandom === 2) ?
-                `Я почти у верен, вы загадали число ${answerNumber }?` : (lessPhraseRandom === 3) ?
-                `Ну и число вы загадали, это ${answerNumber }?` :
-                `Так я могу и проиграть, это число ${answerNumber }?`;
+            (answerNumber>=0) ?
+            answerField.innerText = 'Вы загадали число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Вы загадали число ' + minNumToWords(answerNumber) + ' ?' : (lessPhraseRandom === 2) ?
+            (answerNumber>=0) ?
+            answerField.innerText = 'Я почти у верен, вы загадали число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Я почти у верен, вы загадали число ' + minNumToWords(answerNumber) + ' ?' : (lessPhraseRandom === 3) ?
+            (answerNumber>=0) ?
+            answerField.innerText = 'Ну и число вы загадали, это ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Ну и число вы загадали, это ' + minNumToWords(answerNumber) + ' ?' :
+            (answerNumber>=0) ?
+            answerField.innerText = 'Так я могу и проиграть, это число ' + numToWords(answerNumber) + ' ?'
+            :
+            answerField.innerText = 'Так я могу и проиграть, это число ' + minNumToWords(answerNumber) + ' ?';
             answerField.innerText = lessAnswerPhrase;
         }
     }
 })
+
 
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun) {
@@ -123,3 +153,256 @@ document.getElementById('btnEqual').addEventListener('click', function () {
         gameRun = false;
     }
 })
+
+// let replace = numToWords(answerNumber);
+function minNumToWords(n2){
+    let n3 = -1 * n2;
+    return 'минус ' + numToWords(n3);
+}
+
+function numToWords(n) {
+    let a = n%10;
+    let b = Math.floor(n%100/10);
+    let c = Math.floor(n%1000/100);
+
+
+    Arr1 = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять']; 
+    Arr2 = ['одинадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать',
+    'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
+    Arr3 = ['десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+    Arr4 = ['сто', 'двести', 'тристо', 'четыресто', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+
+    if (n > 0 && n < 10) {
+        return Arr1[a-1] ;
+    }
+    else if (n > 10 && n < 20) {
+        return Arr2[a - 1];
+    }
+    else if (n >= 20 && n < 100) {
+        if (a-1 < 0){return Arr3[b - 1] ;}
+        else {
+            return Arr3[b - 1] + ' ' + Arr1[a - 1];
+        }
+    }
+    else if (n === 0) {
+        return n = 0;
+    }
+    else if (n >= 100 && n < 110)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 110 && n < 120){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 120 && n < 200){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+
+    //For 200
+
+    else if (n >= 200 && n < 210)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 210 && n < 220){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 220 && n < 300){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+     // For 300
+     else if (n >= 300 && n < 310)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 310 && n < 320){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 320 && n < 400){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 400
+    else if (n >= 400 && n < 410)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 410 && n < 420){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 420 && n < 500){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 500
+    else if (n >= 500 && n < 510)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 510 && n < 520){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 520 && n < 600){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 600
+
+    else if (n >= 600 && n < 610)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 610 && n < 620){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 620 && n < 700){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 700
+    else if (n >= 700 && n < 710)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 710 && n < 720){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 720 && n < 800){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 800
+    else if (n >= 800 && n < 810)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 810 && n < 820){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 820 && n < 900){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+
+    //For 900
+    else if (n >= 900 && n < 910)
+    if (a-1 < 0){
+        return Arr4[c-1] ;}
+    else{
+        return Arr4[c-1] + ' ' + Arr1[a -1];
+    }
+
+    else if (n >= 910 && n < 920){
+        if (a-1 < 0){
+            return Arr4[c-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr2[a -1];
+        }
+    }
+
+    else if (n >= 920 && n < 1000){
+        if (a-1 < 0){
+            return Arr4[c-1] + ' ' + Arr3[b-1] ;}
+        else{
+            return Arr4[c-1] + ' ' + Arr3[b-1] + ' ' + Arr1[a - 1];
+        }
+    }
+     
+}  
